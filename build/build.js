e94a95c4885c6cf8c9fae2b15b35a836fe3747c9
Ebru Yeniay <43846395+yeniayeb@users.noreply.github.com>
2021-02-15 21:31:10 +0100

    Add files via upload

--- build/build.js
+++ build/build.js
@@ -74,7 +74,7 @@ var firework;
 (function (firework) {
     // server url local and heroku
     firework.localhostUrl = 'http://127.0.0.1:8081/api/';
-    firework.herokuUrl = "https://firework-exercise.herokuapp.com/api/";
+    firework.herokuUrl = "https://firework-rockets.herokuapp.com/api/";
 })(firework || (firework = {}));
 var firework;
 (function (firework) {
--- configuration.ts
+++ configuration.ts
@@ -1,5 +1,5 @@
 namespace firework {
 // server url local and heroku
 export const localhostUrl: string = 'http://127.0.0.1:8081/api/';
-export const herokuUrl: string = "https://firework-exercise.herokuapp.com/api/";
+export const herokuUrl: string = "https://firework-rockets.herokuapp.com/api/";
 }
